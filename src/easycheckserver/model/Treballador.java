0b81044c1c3b6f4704c81f8b1625a0a148f2fc16
Toni <tonietorres@gmail.com>
2017-10-27 20:14:50 +0200

    JPA a la merdé

--- src/easycheckserver/model/Treballador.java
+++ src/easycheckserver/model/Treballador.java
@@ -8,15 +8,6 @@ package easycheckserver.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
 
 /**
  *
