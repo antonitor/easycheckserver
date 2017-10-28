759ee4c588af70bd72a4038da9de63e567beaed7
Toni <tonietorres@gmail.com>
2017-10-28 14:20:03 +0200

    Gestor de persistencia y DbContract

---
+++ src/easycheckserver/persistencia/DbContract.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package easycheckserver.persistencia;
+
+/**
+ *
+ * @author Toni
+ */
+public class DbContract {
+
+    private DbContract() {
+    }
+
+    public static class TaulaTreballador {
+        public final static String NOM_TAULA = "treballador";
+        public final static String ID = "_id";
+        public final static String NOM = "nom";
+        public final static String COGNOM1 = "cognom1";
+        public final static String COGNOM2 = "cognom2";
+        public final static String ADMIN = "esAdmin";
+        public final static String LOGIN = "login";
+        public final static String PASSWORD = "password";  
+    }
+    
+        public static class TaulaServeis {
+        public final static String NOM_TAULA = "serveis";
+        public final static String ID = "_id"; 
+        public final static String DESCRIPCIO = "descripcio";
+        public final static String DATASERVEI = "data_servei";
+        public final static String HORAINICI = "hora_inici";
+        public final static String HORAFINAL = "hora_final";
+    }
+        
+    public static class TaulaReserva {
+        public final static String NOM_TAULA = "serveis";
+        public final static String ID = "_id"; 
+        public final static String IDSERVEI = "id_servei";
+        public final static String LOCALITZADOR = "localitzador";
+        public final static String DATARESERVA = "data_reserva";
+        public final static String NOM = "nom_titular";
+        public final static String COGNOM1 = "cognom1_titular";
+        public final static String COGNOM2 = "cognom2_titular";
+        public final static String TELF = "telefon_titular";
+        public final static String EMAIL = "email_titular";
+        public final static String QRCODE = "qr_code";
+        public final static String CHECKIN = "checkin";
+        public final static String DNI = "dni_titular";
+    }
+
+}
---
+++ src/easycheckserver/persistencia/GestorPersistencia.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package easycheckserver.persistencia;
+
+import easycheckserver.persistencia.DbContract.TaulaReserva;
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ *
+ * @author Toni
+ */
+public class GestorPersistencia {
+
+    private Connection conn = null;
+    private Driver driver = null;
+    private final String URL = "jdbc:postgresql://localhost:5432/easycheck";
+    private final String USER = "marcarniuser";
+    private final String PASS = "marcarnipass";
+
+    public GestorPersistencia() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC Controller Not Found!!");
+        }
+
+    }
+
+    public void open() {
+        try {
+            driver = DriverManager.getDriver(URL);
+            Properties properties = new Properties();
+            properties.setProperty("user", USER);
+            properties.setProperty("password", PASS);
+            conn = driver.connect(URL, properties);
+        } catch (SQLException ex) {
+            System.out.println(ex.getErrorCode());
+        }
+    }
+
+    public void close() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getErrorCode());
+        }
+    }
+
+    public void getReserves() throws SQLException {
+        PreparedStatement stm = null;
+        ResultSet rs;
+        try {
+            stm = conn.prepareStatement("SELECT * FROM " + TaulaReserva.NOM_TAULA);
+            rs = stm.executeQuery();
+        } finally {
+            closeStatement(stm);
+        }
+        
+        while (rs.next()) {
+            int id = rs.getInt(TaulaReserva.ID);
+            //en construcción
+        }
+        
+    }
+
+    public void closeStatement(PreparedStatement stm) {
+        try {
+            if (stm != null && !stm.isClosed()) {
+                stm.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getErrorCode());
+        }
+    }
+    
+
+}
